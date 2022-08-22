e1ae82077ee72087ebd47300596867522afa4edd
mynameisjeff99 <jeff.guoshengc@gmail.com>
2022-08-22 05:10:17 -0400

    create the grid using React

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import {Canvas} from './Canvas';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: ''};
+    //this.state = { apiResponse: ''};
   }
 
-  callAPI() {
-    fetch('http://localhost:9000/testAPI')
+  render() {
+    return (
+      <div>
+        <Canvas title='Home Page' />
+      </div>
+    )
+  }
+
+  /*callAPI() {
+    fetch("http://localhost:9000/testAPI")
       .then(res => res.text())
       .then(res => this.setState({apiResponse: res}));
   }
@@ -23,6 +32,6 @@ class App extends Component {
         <p className="App-intro">;{this.state.apiResponse}</p>
       </div>
     )
-  }
+  }*/ß
 }
 export default App;
---
+++ client/src/Canvas.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Canvas extends Component {
+  constructor(props) {
+    super(props);
+    this.changeBackground = this.changeBackground.bind(this);
+  }
+
+  changeBackground(e) {
+    e.target.style.background = 'red';
+  }
+
+  render() {
+    const {title} = this.props;
+    let all_items =[];
+    for (let i = 0; i < 16; i++) {
+      let items = []
+      for (let j = 0; j < 16; j++) {
+        let id= i.toString()+'_'+j.toString()
+        items.push(<div onMouseOver={this.changeBackground} 
+          id={id} key={j} className='canvasBox'/>)
+      }
+      all_items.push(<div key={i} className='canvasRow row'> {items} </div>);
+    }
+
+    return (
+      <div id='container' className='colFlex'>
+        <h1>{title}</h1>
+        <div id='canvas' className='colFlex'>
+          {all_items}
+        </div>
+      </div>
+    )
+  }
+}
+export {Canvas};
